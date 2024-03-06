345bc5250569dd82088b12b3689d835961b1fa1f
Savecoders <pablopincay2003@gmail.com>
2024-03-06 11:18:47 -0500

    Add: new feature tags

--- api/src/products/dto/create-product.dto.ts
+++ api/src/products/dto/create-product.dto.ts
@@ -41,6 +41,11 @@ export class CreateProductDto {
   @IsArray()
   sizes: string[];
 
+  @IsString({ each: true })
+  @IsArray()
+  @IsOptional()
+  tags?: string[];
+
   @IsIn(['men', 'women', 'kids', 'unisex'])
   gender: string;
 }
