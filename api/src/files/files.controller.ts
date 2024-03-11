c4f28807e017c87dcc99522388e215f3d3b0a8c7
Savecoders <pablopincay2003@gmail.com>
2024-03-11 16:32:16 -0500

    Add: File services

---
+++ api/src/files/files.controller.ts
@@ -0,0 +1,25 @@
+import {
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadProductFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+    return {
+      file: file.originalname,
+    };
+  }
+}
