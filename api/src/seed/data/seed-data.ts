963eb889407b7499b5cd98953a322ba9687ccc0e
Savecoders <pablopincay2003@gmail.com>
2024-03-18 16:54:42 -0500

    Add: create users in seed

--- api/src/seed/data/seed-data.ts
+++ api/src/seed/data/seed-data.ts
@@ -1,3 +1,4 @@
+import * as bcrypt from 'bcrypt';
 interface SeedProduct {
   description: string;
   images: string[];
@@ -14,11 +15,33 @@ interface SeedProduct {
 type ValidSizes = 'XS' | 'S' | 'M' | 'L' | 'XL' | 'XXL' | 'XXXL';
 type ValidTypes = 'shirts' | 'pants' | 'hoodies' | 'hats';
 
+interface SeedUser {
+  email: string;
+  name: string;
+  password: string;
+  roles: string[];
+}
+
 interface SeedData {
+  users: SeedUser[];
   products: SeedProduct[];
 }
 
 export const initialData: SeedData = {
+  users: [
+    {
+      email: 'esteesunemail@gmail.com',
+      name: 'Fernando Doe',
+      password: bcrypt.hashSync('password123', 10),
+      roles: ['admin'],
+    },
+    {
+      email: 'niideadeq@gmail.com',
+      name: 'Dana Herrera',
+      password: bcrypt.hashSync('password123', 10),
+      roles: ['user'],
+    },
+  ],
   products: [
     {
       description:
--- api/src/seed/seed.service.ts
+++ api/src/seed/seed.service.ts
@@ -1,28 +1,57 @@
 import { Injectable } from '@nestjs/common';
-import { ProductsService } from 'src/products/products.service';
+import { ProductsService } from '../products/products.service';
 import { initialData } from './data/seed-data';
+import { User } from '../auth/entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SeedService {
-  constructor(private readonly productService: ProductsService) {}
+  constructor(
+    private readonly productService: ProductsService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
 
   async runSeed() {
-    await this.insertNewProducts();
+    await this.deleteTables();
+    const userToSeed = await this.insertUsers();
+    await this.insertNewProducts(userToSeed);
     return `return seed executed!`;
   }
 
-  private async insertNewProducts() {
+  private async insertNewProducts(user: User) {
     await this.productService.deletAllProducts();
     const seedProducts = initialData.products;
 
     const insertPromises = [];
 
     seedProducts.forEach((product) => {
-      insertPromises.push(this.productService.create(product));
+      insertPromises.push(this.productService.create(product, user));
     });
 
     await Promise.all(insertPromises);
 
     return true;
+  }
+
+  private async insertUsers() {
+    const seedUsers = initialData.users;
+
+    const users: User[] = [];
+
+    seedUsers.forEach((user) => {
+      users.push(this.userRepository.create(user));
+    });
+
+    const dbUsers = await this.userRepository.save(seedUsers);
+
+    return dbUsers[0];
+  }
+
+  private async deleteTables() {
+    await this.productService.deletAllProducts();
+    const queryBuilder = this.userRepository.createQueryBuilder();
+    await queryBuilder.delete().where({}).execute();
   }
 }
